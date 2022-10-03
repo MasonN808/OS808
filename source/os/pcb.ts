45bf0cfce63013d8dc025c655136ffe732a1523f
MasonN808 <mason.naka@gmail.com>
2022-10-03 14:04:04 -0400

    Added some pointers for PCB

--- source/os/pcb.ts
+++ source/os/pcb.ts
@@ -2,7 +2,15 @@
 module TSOS {
     export class Pcb {
 
-        constructor() {
+        constructor(
+            public processState: string = "",
+            public programCounter: number = 0,
+            public intermediateRepresentation: string = "",
+            public Xreg: number = 0,
+            public Yreg: number = 0,
+            public Zreg: number = 0,
+            public location: string = "",
+            public priority: string = "")  {
         }
     }
 }

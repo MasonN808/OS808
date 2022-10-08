8dc8c9ac9b36c9054e3a935f134ae157c3fffd83
MasonN808 <mason.naka@gmail.com>
2022-10-08 18:04:37 -0400

    Added more Op codes

--- distrib/host/cpu.js
+++ distrib/host/cpu.js
@@ -37,21 +37,58 @@ var TSOS;
             // Do the real work here. Be sure to set this.isExecuting appropriately.
             // Get the Op code given the pid and pc
             var opCode = TSOS.MemoryAccessor.readMemory(this.PID, this.PC);
+            // // Get the constant one op code above current PC (i.e, do one op code lookahead)
+            // const lookAheadConstant1 = TSOS.MemoryAccessor.readMemory(this.PID, this.PC + 1);
+            // // Get the constant two op codes above current PC (i.e, do two op code lookahead)
+            // const lookAheadConstant2 = TSOS.MemoryAccessor.readMemory(this.PID, this.PC + 2);
+            const pcb = _MemoryManager.PIDMap.get(this.PID)[1];
             // Have a massive switch statement for all possible Op codes
             switch (opCode) {
                 // Load the accumulator with a constant
                 case ("A9"):
-                    // Get the constant (i.e, do one op code lookahead)
-                    const constant = TSOS.MemoryAccessor.readMemory(this.PID, this.PC + 1);
+                    // Get the constant one op code above current PC (i.e, do one op code lookahead)
+                    const lookAheadConstant = TSOS.MemoryAccessor.readMemory(this.PID, this.PC + 1);
                     // parse it to an int to store in accumulator
-                    const paresedConstant = parseInt(constant, 16);
+                    const paresedConstant = parseInt(lookAheadConstant, 16);
                     // Assign the parsed constant to the CPU and PCB
                     this.Acc = paresedConstant;
-                    _MemoryManager.PIDMap.get(this.PID)[1].Acc = paresedConstant;
+                    pcb.Acc = paresedConstant;
+                    // Increase the PC in CPU and PCB
+                    this.increasePC(2);
+                    break;
+                // Load the accumulator from memory
+                case ("AD"):
+                    // Get the storage location one op code above current PC (i.e, do one op code lookahead)
+                    var lookAheadStorageLocation = parseInt(TSOS.MemoryAccessor.readMemory(this.PID, this.PC + 1), 16);
+                    // Retrieve the constant from specified memory location
+                    const retrievedConstant = TSOS.MemoryAccessor.readMemory(this.PID, lookAheadStorageLocation);
+                    this.Acc = parseInt(retrievedConstant, 16);
+                    pcb.Acc = parseInt(retrievedConstant, 16);
+                    this.increasePC(2);
+                    break;
+                // Store the accumulator in memory
+                case ("8D"):
+                    // Convert int back to hex
+                    var hexAcc = this.Acc.toString(16);
+                    // Check if we need to append leading 0
+                    if (hexAcc.length === 1) {
+                        hexAcc = "0" + hexAcc;
+                    }
+                    // Get the storage location one op code above current PC (i.e, do one op code lookahead)
+                    var lookAheadStorageLocation = parseInt(TSOS.MemoryAccessor.readMemory(this.PID, this.PC + 1), 16);
+                    // Write the accumulator at the specified storage location
+                    TSOS.MemoryAccessor.writeMemory(this.PID, lookAheadStorageLocation, hexAcc);
+                    // Increase the PC in CPU and PCB
+                    this.increasePC(3);
                     break;
             }
             // Now update the displayed PCB
             TSOS.Control.hostProcesses(this.PID);
+        }
+        increasePC(pcIncrease) {
+            const pcb = _MemoryManager.PIDMap.get(this.PID)[1];
+            this.PC += pcIncrease;
+            pcb.programCounter += pcIncrease;
         }
     }
     TSOS.Cpu = Cpu;
--- source/host/cpu.ts
+++ source/host/cpu.ts
@@ -43,11 +43,6 @@ module TSOS {
             // Get the Op code given the pid and pc
             var opCode = TSOS.MemoryAccessor.readMemory(this.PID, this.PC);
 
-            // // Get the constant one op code above current PC (i.e, do one op code lookahead)
-            // const lookAheadConstant1 = TSOS.MemoryAccessor.readMemory(this.PID, this.PC + 1);
-            // // Get the constant two op codes above current PC (i.e, do two op code lookahead)
-            // const lookAheadConstant2 = TSOS.MemoryAccessor.readMemory(this.PID, this.PC + 2);
-
             const pcb = _MemoryManager.PIDMap.get(this.PID)[1];
 
             // Have a massive switch statement for all possible Op codes
@@ -55,26 +50,141 @@ module TSOS {
                 // Load the accumulator with a constant
                 case ("A9"):
                     // Get the constant one op code above current PC (i.e, do one op code lookahead)
-                    const lookAheadConstant1 = TSOS.MemoryAccessor.readMemory(this.PID, this.PC + 1);
-                    // parse it to an int to store in accumulator
-                    const paresedConstant = parseInt(lookAheadConstant1, 16);
+                    var constant = TSOS.MemoryAccessor.readMemory(this.PID, this.PC + 1);
 
-                    // Assign the parsed constant to the CPU and PCB
-                    this.Acc = paresedConstant;
-                    pcb.Acc = paresedConstant;
+                    // Update the Accumulator in CPU and PCB
+                    this.updateAcc(constant)
 
                     // Increase the PC in CPU and PCB
-                    this.PC += 2;
-                    pcb.programCounter += 2;
+                    this.increasePC(2);
+                    break;
+                
+                // Load the accumulator from memory
+                case ("AD"):
+                    // Get the storage location one op code above current PC (i.e, do one op code lookahead)
+                    var storageLocation = parseInt(TSOS.MemoryAccessor.readMemory(this.PID, this.PC + 1), 16);
+
+                    // Query constant from memory location
+                    var constantInMemory = TSOS.MemoryAccessor.readMemory(this.PID, storageLocation);
+                    
+                    // Update the Accumulator in CPU and PCB
+                    this.updateAcc(constantInMemory)
+
+                    this.increasePC(2);
                     break;
 
                 // Store the accumulator in memory
                 case ("8D"):
+                    // Convert int back to hex
+                    var hexAcc = this.Acc.toString(16);
+
+                    // Check if we need to append leading 0
+                    if (hexAcc.length === 1) {
+                        hexAcc = "0" + hexAcc;
+                    }
+
+                    // Get the storage location one op code above current PC (i.e, do one op code lookahead)
+                    var storageLocation = parseInt(TSOS.MemoryAccessor.readMemory(this.PID, this.PC + 1), 16);
+                    
+                    // Write the accumulator at the specified storage location
+                    TSOS.MemoryAccessor.writeMemory(this.PID, storageLocation, hexAcc)
+
+                    // Increase the PC in CPU and PCB
+                    this.increasePC(3);
+                    break;
                 
+                // Load the X-register with a constant
+                case ("A2"):
+                    // Get the constant one op code above current PC (i.e, do one op code lookahead)
+                    var constant = TSOS.MemoryAccessor.readMemory(this.PID, this.PC + 1);
+                    
+                    this.updateX(constant);
+
+                    // Increase the PC in CPU and PCB
+                    this.increasePC(2);
+                    break;
+                
+                // Load the X-register from memory
+                case ("AE"):
+                    // Get the storage location one op code above current PC (i.e, do one op code lookahead)
+                    var storageLocation = parseInt(TSOS.MemoryAccessor.readMemory(this.PID, this.PC + 1), 16);
+                    
+                    // Query constant from memory location
+                    var constantInMemory = TSOS.MemoryAccessor.readMemory(this.PID, storageLocation);
+                        
+                    // Update the Accumulator in CPU and PCB
+                    this.updateX(constantInMemory);
+
+                    this.increasePC(2);
+                    break;
+                
+                // Load the Y-register with a constant
+                case ("A0"):
+                    // Get the constant one op code above current PC (i.e, do one op code lookahead)
+                    var constant = TSOS.MemoryAccessor.readMemory(this.PID, this.PC + 1);
+                    
+                    this.updateY(constant);
+
+                    // Increase the PC in CPU and PCB
+                    this.increasePC(2);
+                    break;
+                
+                // Load the Y-register from memory
+                case ("AC"):
+                    // Get the storage location one op code above current PC (i.e, do one op code lookahead)
+                    var storageLocation = parseInt(TSOS.MemoryAccessor.readMemory(this.PID, this.PC + 1), 16);
+                    
+                    // Query constant from memory location
+                    var constantInMemory = TSOS.MemoryAccessor.readMemory(this.PID, storageLocation);
+                        
+                    // Update the Accumulator in CPU and PCB
+                    this.updateY(constantInMemory);
+
+                    this.increasePC(2);
+                    break;
+                
+                // No operation
+                case ("EA"):
+                    this.increasePC(1);
+                    break;
+
+                // Compare a byte in memory to the X-register;  Sets the Z (zero) flag if equal
+                // case ("EC"):
 
             }
             // Now update the displayed PCB
             TSOS.Control.hostProcesses(this.PID);
+        }
+
+        private increasePC(pcIncrease: number): void {
+            const pcb = _MemoryManager.PIDMap.get(this.PID)[1];
+            
+            this.PC += pcIncrease;
+            pcb.programCounter += pcIncrease;
+        }
+
+        private updateAcc(newAccAsHex: string): void {
+            const pcb = _MemoryManager.PIDMap.get(this.PID)[1];
+
+            // parse string to an int to store in accumulator
+            this.Acc = parseInt(newAccAsHex, 16);
+            pcb.Acc = parseInt(newAccAsHex, 16);
+        }
+
+        private updateX(newXAsHex: string): void {
+            const pcb = _MemoryManager.PIDMap.get(this.PID)[1];
+
+            // parse string to an int to store in accumulator
+            this.Acc = parseInt(newXAsHex, 16);
+            pcb.Acc = parseInt(newXAsHex, 16);
+        }
+
+        private updateY(newYAsHex: string): void {
+            const pcb = _MemoryManager.PIDMap.get(this.PID)[1];
+
+            // parse string to an int to store in accumulator
+            this.Acc = parseInt(newYAsHex, 16);
+            pcb.Acc = parseInt(newYAsHex, 16);
         }
     }
 }
